bd8394709e6e1bf69240202daca06a6a83769298
Idris Babay <idrissbabay@gmail.com>
2025-02-02 19:46:42 +0100

    init and configure springboot project

---
+++ src/main/java/com/interviews/banking_java_sql_git_docker/BankingJavaSqlGitDockerApplication.java
@@ -0,0 +1,13 @@
+package com.interviews.banking_java_sql_git_docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingJavaSqlGitDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingJavaSqlGitDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interviews/banking_java_sql_git_docker/BankingJavaSqlGitDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interviews.banking_java_sql_git_docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingJavaSqlGitDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
